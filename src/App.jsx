b2320b0bd849cb933138b728d303f6e72647cba3
Zack VenHuizen <78177579+zvenhuizen@users.noreply.github.com>
2021-08-14 23:16:13 -0500

    Changed stateless function to stateful class

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './styles/main.scss';
 import Header from './Header.js';
+import React from 'react';
 
 /* ==== COMMENTS ===== */
 // I will add a lot of comments in code to help us keep track of what each person is doing, and why.
@@ -14,6 +15,8 @@ import Header from './Header.js';
 // Change App from a stateless functional componenet to a stateful class based component.
 // The App should look the same after the change.
 
+//Below is the original code as reference. I have taken the challenge to change this to a stateful class based component
+/*
 function App() {
   return (
     <div className="App">
@@ -34,6 +37,34 @@ function App() {
       </header>
     </div>
   );
+}
+*/
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return(
+      <div className="App">
+        <Header />
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
