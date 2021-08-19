68a11ba69d5311e6403caf826a665dc34c0a0b9d
Keith VenHuizen <keith.venhuizen@gmail.com>
2021-08-18 23:11:42 -1000

    Add comments for DiceInput event handling

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -6,28 +6,42 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      style: {
-        backgroundColor: '#232323'
-      } 
+      
+      // Add state of diceInputValue set to an empty string
+      // Add state of rolledDice set to an empty string
     }
-    this.handleClick = this.handleClick.bind(this);
+    // Bind handleDiceInputChange to this
+    // Bind handleRollDiceClick to this
   }
-  handleClick() {
-    const red = Math.floor(Math.random() * 99);
-    const green = Math.floor(Math.random() * 99);
-    const blue = Math.floor(Math.random() * 99);
-    this.setState({
-      style: {
-        backgroundColor: `#${red}${green}${blue}`
-      }
-    })
-  }
+  // define a handleDiceInputChange function
+  // this function should handle validation before changing state
+  // if handling the validation globally:
+  // first, I'd create file in the helpers directory (similar to FormErrors).
+  // that file should export two functions validateLength and validateDiceInput(or whatever you want to call it)
+  // import the above functions and call them here passing in the event.target.value 
+  // if validation fails, you just update state with the pervious state instead of with the new string
+  // otherwise you update state with event.target.valu
+  // if handlling the validation locally: 
+  // this function should probably take two arguments
+  // a boolean that states whether or not validation passed
+  // and event.target.value
+  // you need this so you can update the state with either the new sting or the old string and render the right value in DiceInput
+
+  // define a handleRollDiceClick function
+  // this function should update rolledDice in state and reset diceInputValue to an empty string
+
   render() {
+
     return(
       <div className="App">
 
         <Header style={this.state.style} headerClick={this.handleClick}/>
         <br />
+        {/* Add value(or whatever you want to name it) prop to DiceInput and set it equal to diceInputValue of this.state */}
+        {/* Add rollDiceClick(or whatever you want to name it) prop to DiceInput and set it equal to handler function */}
+        {/* Add diceInputChange(or whatever you want to name it) prop to DiceInput and set it equal to handler function */}
+        {/* If you want to alert the user on errors, add error prop to DiceInput ans set it equal to error array */}
+        {/* if you decide to handle validation locally, ignore the above line */}
         <DiceInput />
         
       </div>
--- src/probability/DiceInput.jsx
+++ src/probability/DiceInput.jsx
@@ -8,14 +8,28 @@ class DiceInput extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            // remove value from state as this will be handled globall in App
             value: ''
+            // add errors state and set it to an empty array or object (depends on how you're outputting your errors)
         }
 
         this.handleUserInput = this.handleUserInput.bind(this);
         this.handleRollClick = this.handleRollClick.bind(this);
     }
 
+    // if you want to handle validation locally, I'd change this name to evidence that.
+    // maybe name the function validateInput or something similar
+    // then set the onChange prop of the input field equal to validateInput (or whatever you named it)
     handleUserInput = (e) => {
+
+        // if handling validation locally, I would outsource the validation to a separate file and call them in this function
+
+        // first, I'd create file in the helpers directory (similar to FormErrors).
+        // that file should export two functions validateLength and validateDiceInput(or whatever you want to call it)
+        // import the above functions and call them here passing in the event.target.value 
+
+        // you can then call props.diceInputChange(or whatever you named the prop) and pass it the true of false base on whether validations passed and event.target.value
+        // this will update state, which will update your input
         const {name, value}= e.target;
         let currentFormErrors = this.state.currentFormErrors;
 
@@ -44,6 +58,14 @@ class DiceInput extends Component {
         return (
             <div>
                 <legend>Dice combination:</legend>
+
+                {/* set input onChange have it call props.diceInputChange(or whatever you named the prop) and pass it the event to activate the global handleChange */}
+                {/* see comment in handleUserInput if you'd rather handle validation locally (which I think may make the most sense) */}
+                {/* use the value prop and set it equal to props.value(or whatever you named the prop) to get the value from global state */}
+                {/* conditionally add a p element that lists the errors above the input field if there are errors*/}
+                {/* because there you won't update the input if validation fails, you don't have to style the input for an error, because you will just prevent the error */}
+                {/* but it is a good idea to let the user know that they aren't seeing what they typed because it didn't pass validation */}
+                {/* that's why I'd add the p element with an explanation of the error */}
                 <input 
                   className={this.state.currentFormErrors && this.state.currentFormErrors.dice
                     ? 'form-control error'
@@ -53,6 +75,8 @@ class DiceInput extends Component {
                   noValidate 
                   onBlur={this.handleUserInput}
                 />
+
+                {/* set the onClick event equal to props.rollDiceClick(or whatever you named it) to activate the global handleClick */}
                 <button
                   className='btn btn-submit'
                   disabled={Object.entries(this.state.currentFormErrors || {}).length > 0}
