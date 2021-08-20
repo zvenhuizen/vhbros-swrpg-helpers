d5dfc45e919aab9f455049e8846c284a2a1f1df3
Keith VenHuizen <keith.venhuizen@gmail.com>
2021-08-19 21:25:45 -1000

    Comments on Validation progress

--- src/helpers/validateInput.js
+++ src/helpers/validateInput.js
@@ -1,9 +1,10 @@
 // Allowed keyCodecodes: 66 71 75 80 82 83 84 86 87 89 33 45 42 36
-export function validInput(event) {
+
+export function validInput(event) {// since you're currently not passing and event, but rather a string, consider changing event to text or string or something more accurately descriptive
     let valid = false
-    console.log(event.keyCode);
+    console.log(event.keycode);
     switch (event.keyCode) {
-        case event.keyCode === 33:
+        case event.keyCode === 33: // you should be able to remove the event.keyCode === part of this, and just say 33...
         case event.keyCode === 36:
         case event.keyCode === 42:
         case event.keyCode === 45:
@@ -26,6 +27,7 @@ export function validInput(event) {
     return valid;
 }
 
+// I don't think you're done writing this yet, but if you are, I know why it isn't working...
 export function validLength(text) {
     let valid = false;
     if (text.length >= 1 && text.length <= 24) 
