2ed7b7b4fa4b63e7d879396d3eb9baa8d37e5fa5
Zack VenHuizen <78177579+zvenhuizen@users.noreply.github.com>
2021-11-30 16:49:05 -0600

    fix firebase config

---
+++ src/firebase-config.js
@@ -0,0 +1,19 @@
+const config = {
+    apiKey: "AIzaSyAIrETYx861kemsmmYGBNc_q8yamLZe8rw",
+    authDomain: "vhbros-swrpg-helpers.firebaseapp.com",
+    databaseURL: "https://vhbros-swrpg-helpers-default-rtdb.firebaseio.com",
+    projectId: "vhbros-swrpg-helpers",
+    storageBucket: "vhbros-swrpg-helpers.appspot.com",
+    messagingSenderId: "523300652788",
+    appId: "1:523300652788:web:1ee68b04304bb2eda600a1",
+    measurementId: "G-WMP88F6S7XW"
+};
+
+export function getFirebaseConfig() {
+    if(!config || !config.apiKey) {
+        throw new Error('No Firebase configuration object provided.' + '\n' +
+        'Add your web app\'s configuration object to firebase-config.js');
+    } else {
+        return config;
+    }
+}
--- src/index.js
+++ src/index.js
@@ -3,22 +3,12 @@ import ReactDOM from 'react-dom';
 import './assets/styles/main.scss';
 import App from './app/App';
 import reportWebVitals from './helpers/reportWebVitals';
+import { getFirebaseConfig } from './firebase-config';
 import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore/lite';
+import { getFirestore } from 'firebase/firestore';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAIrETYx861kemsmmYGBNc_q8yamLZe8rw",
-  authDomain: "vhbros-swrpg-helpers.firebaseapp.com",
-  databaseURL: "https://vhbros-swrpg-helpers-default-rtdb.firebaseio.com",
-  projectId: "vhbros-swrpg-helpers",
-  storageBucket: "vhbros-swrpg-helpers.appspot.com",
-  messagingSenderId: "523300652788",
-  appId: "1:523300652788:web:1ee68b04304bb2eda600a1",
-  measurementId: "G-WMP88F67XW"
-};
-
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const firebaseAppConfig = getFirebaseConfig();
+initializeApp(firebaseAppConfig);
 
 ReactDOM.render(
   <React.StrictMode>
