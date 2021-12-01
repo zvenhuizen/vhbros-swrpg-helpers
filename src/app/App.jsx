cb6a6d8e73a56d9be82c5ad8d4958f4bcf6e9a3f
Zack VenHuizen <78177579+zvenhuizen@users.noreply.github.com>
2021-11-30 23:37:31 -0600

    allow for final odds to be updated in state

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,15 +2,13 @@ import React from 'react';
 import Header from './Header';
 import RollResults from '../probability/RollResults';
 import DiceResults from '../probability/DiceResults';
-import OddsResults from '../probability/OddsResults';
 import DiceInput from '../probability/DiceInput';
 import cancelResults from '../helpers/cancelResults';
 import rollDice from '../helpers/rollDice';
 import calculateSuccessProb from '../helpers/calculateSuccessProbability';
 import successOdds from '../probability/SuccessOdds';
-import rollOdds from '../probability/rollOdds';
 import {validLength} from '../helpers/validateInput'
-import calculateAdvantageProb from '../helpers/calculateAdvantageProbability';
+import GetOdds from '../probability/getOdds';
 
 class App extends React.Component {
   constructor(props) {
@@ -20,10 +18,8 @@ class App extends React.Component {
       diceResult: [],
       rollResult: [],
       rolledDice: '',
-      sucOdds: '--.--',
-      advOdds: '--.--',
-      rollOdds: '--.--',
       successOdds: '--.--',
+      finalOdds: '--.--',
       errors: []
     }
 
@@ -45,47 +41,36 @@ class App extends React.Component {
       rollResult: [],
       errors: [],
       rolledDice: '',
-      sucOdds: '--.--',
-      advOdds: '--.--',
-      rollOdds: '--.--',
-      successOdds: sucPct
+      successOdds: sucPct,
+      finalOdds: '--.--'
     });
   }
 
   handleRollClick(event) {
     // Validate length to ensure there is at least 1 die to roll
-    let sucPct = '--.--';
-    let advPct = '--.--';
-    let oddsPct = '--.--';
     let errors = [];
 
     if(validLength(this.state.diceInputValue,1,24)) {
       const diceResult = rollDice(this.state.diceInputValue);
       const rollResult = cancelResults(diceResult);
-      sucPct = (rollOdds(calculateSuccessProb(this.state.diceInputValue), cancelResults(diceResult), 'success')*100).toFixed(2);
-      advPct = (rollOdds(calculateAdvantageProb(this.state.diceInputValue), cancelResults(diceResult), 'advantage')*100).toFixed(2);
-      oddsPct = ((sucPct/100) * (advPct/100) * 100).toFixed(2);
+      const finalOdds = GetOdds(this.state.diceInputValue,diceResult)
     
       this.setState({
         diceResult: diceResult,
         rollResult: rollResult,
-        rolledDice: this.state.diceInputValue,
-        sucOdds: sucPct,
-        advOdds: advPct,
-        rollOdds: oddsPct,
-        diceInputValue: '',
+        finalOdds: finalOdds,
+        diceInputValue: ''
       })
     } else {
       errors.push("Cannot roll 0 dice");
       
       this.setState({
         successOdds: '--.--',
+        finalOdds: '--.--',
         diceResult: [],
         rollResult: [],
         rolledDice: '',
-        sucOdds: '--.--',
-        advOdds: '--.--',
-        rollOdds: '--.--',
+        diceInputValue: '',
         errors: errors
       })
     }
@@ -98,12 +83,24 @@ class App extends React.Component {
 
         <Header style={this.state.style} />
         <p className='input-errors'>{this.state.errors.map((error,index) => (<span key={index} className="alert alert-danger">{error}</span>))}</p>
-        <DiceInput value={this.state.diceInputValue} diceInputChange={this.handleDiceInput} rollDiceClick={this.handleRollClick} autofocus/>
+        <DiceInput
+          value={this.state.diceInputValue}
+          diceInputChange={this.handleDiceInput}
+          rollDiceClick={this.handleRollClick}
+          autofocus />
         
         <div className='results-container'>
-          <DiceResults results={this.state.diceResult} rolledDice={this.state.rolledDice} dice={this.state.diceInputValue} successChance={this.state.successOdds} />
-          <RollResults results={this.state.rollResult} rolledDice={this.state.rolledDice}/>
-          <OddsResults successChance={this.state.sucOdds} advantageChance={this.state.advOdds} oddsChance={this.state.rollOdds}/>
+
+          <DiceResults 
+            results={this.state.diceResult}
+            rolledDice={this.state.rolledDice}
+            dice={this.state.diceInputValue}
+            successChance={this.state.successOdds} />
+          <RollResults
+            finalOdds={this.state.finalOdds}
+            results={this.state.rollResult}
+            rolledDice={this.state.rolledDice} />
+
         </div>
       </div>
     );
