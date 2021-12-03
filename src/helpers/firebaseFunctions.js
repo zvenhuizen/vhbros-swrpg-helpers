824ff4daabc187dddf1c31bce5a6ed94ddb3d2af
Zack VenHuizen <78177579+zvenhuizen@users.noreply.github.com>
2021-12-02 23:14:33 -0600

    split out load rolls func

--- src/helpers/firebaseFunctions.js
+++ src/helpers/firebaseFunctions.js
@@ -1,17 +1,23 @@
-import { setDoc } from "firebase/firestore";
-import {
-  getFirestore,
-//  collection,
-//  addDoc,
-  query,
-//  orderBy,
-//  limit,
-  onSnapshot,
-//  setDoc,
-//  updateDoc,
-  doc,
-//  serverTimestamp,
-} from 'firebase/firestore';
+//import { setDoc }           from "firebase/firestore";
+import { getFirestore,
+        //collection,
+        //addDoc,
+        query,
+        //orderBy,
+        //limit,
+        onSnapshot,
+        //setDoc,
+        //updateDoc,
+        doc,
+        //serverTimestamp
+      }                     from 'firebase/firestore';
+import { //getStorage,
+        //ref,
+        //uploadBytesResumable,
+        //getDownloadURL,
+        }                   from 'firebase/storage';
+//import { getMessaging, getToken, onMessage } from 'firebase/messaging';
+//import { getPerformance } from 'firebase/performance';
 
 export function rollExists() {
 
@@ -23,6 +29,10 @@ export function getRoll(roll) {
   // returns a snapshot of the db
   const rollQuery = query(doc(getFirestore(), 'rolls', roll));
 
+  /* what Keith initially did worked:
+      onSnapshot(sampleRollQuery, function(snapshot) {
+      console.log(`TOTAL OUTCOMES FOR A BLUE DIE: ${Object.keys(snapshot.data()).length}`)
+  */
   // listen to the query defined above and return what you define in the function(snapshot)
   const result = onSnapshot(rollQuery, (snapshot) => {
     snapshot.data()
