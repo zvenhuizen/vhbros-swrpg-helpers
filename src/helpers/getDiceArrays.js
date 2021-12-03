38ebf1c59f29d438f7330c3e6d845cf21d18d438
Zack VenHuizen <78177579+zvenhuizen@users.noreply.github.com>
2021-12-02 23:15:09 -0600

    get dice arrays for combine

---
+++ src/helpers/getDiceArrays.js
@@ -0,0 +1,39 @@
+import { diceCombos } from './Combos';
+import getDiceSplit from './diceSplit'
+
+export default function DiceArrays(dice) {
+
+  let posDice = getDiceSplit(dice).posDice.split('')
+  let negDice = getDiceSplit(dice).negDice.split('')
+
+  let posDiceArray = posDice.map(sides => {
+    switch(sides) {
+      case 'y':
+        return diceCombos.yellow;
+      case 'g':
+        return diceCombos.green;
+      case 'b':
+        return diceCombos.blue;
+      default:
+        return null;
+    }
+  });
+
+  let negDiceArray = negDice.map(sides => {
+    switch(sides) {
+      case 'r':
+        return diceCombos.red;
+      case 'p':
+        return diceCombos.purple;
+      case 'k':
+        return diceCombos.black;
+      default:
+        return null;
+    }
+  });
+
+  posDiceArray = posDiceArray.filter(e => e != null);
+  negDiceArray = negDiceArray.filter(e => e != null);
+
+  return [posDiceArray, negDiceArray];
+}
