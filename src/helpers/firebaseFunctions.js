fdb66739732acf55514fbfd59284d860d459f912
Zack VenHuizen <78177579+zvenhuizen@users.noreply.github.com>
2022-03-09 14:29:41 -0600

    Fix single die roll error

--- src/helpers/firebaseFunctions.js
+++ src/helpers/firebaseFunctions.js
@@ -23,50 +23,86 @@ export async function getDicePoolData(dicePoolObject) {
   console.log(dicePoolObject);
   let dicePoolData;
 
-  try {
-    // access posRes and negRes asynchronously, but wait until both are finished to continue (can be expanded to include force and nonDice if necessary)
-    let [posDicePoolData, negDicePoolData] = await Promise.all([getDoc(doc(db, 'dicePools', dicePoolObject.posDicePool)), getDoc(doc(db, 'dicePools', dicePoolObject.negDicePool))]);
+  if(dicePoolObject.posDicePool && dicePoolObject.negDicePool) {
+    try {
+      // access posRes and negRes asynchronously, but wait until both are finished to continue (can be expanded to include force and nonDice if necessary)
+      let [posDicePoolData, negDicePoolData] = await Promise.all([getDoc(doc(db, 'dicePools', dicePoolObject.posDicePool)), getDoc(doc(db, 'dicePools', dicePoolObject.negDicePool))]);
 
-    let rollPossibilities = dicePoolDataExists({posDicePoolData: posDicePoolData, negDicePoolData: negDicePoolData}, dicePoolObject)
-    dicePoolData = {posDicePoolObj: rollPossibilities.posDicePoolData, negDicePoolObj: rollPossibilities.negDicePoolData, forceDice: '', symbols: ''};
-  }
-  catch(e) {
-    console.log(e);
-  }
+      let rollPossibilities = dicePoolDataExists({posDicePoolData: posDicePoolData, negDicePoolData: negDicePoolData}, dicePoolObject)
+      dicePoolData = {posDicePoolObj: rollPossibilities.posDicePoolData, negDicePoolObj: rollPossibilities.negDicePoolData, forceDice: '', symbols: ''};
+    }
+    catch(e) {
+      console.log(e);
+    }
 
-  return dicePoolData;
+    return dicePoolData;
+  } else if(dicePoolObject.posDicePool) {
+    try {
+      // access posRes and negRes asynchronously, but wait until both are finished to continue (can be expanded to include force and nonDice if necessary)
+      let [posDicePoolData] = await Promise.all([getDoc(doc(db, 'dicePools', dicePoolObject.posDicePool))]);
+
+      let rollPossibilities = dicePoolDataExists({posDicePoolData: posDicePoolData}, dicePoolObject)
+      dicePoolData = {posDicePoolObj: rollPossibilities.posDicePoolData, negDicePoolObj: '', forceDice: '', symbols: ''};
+    }
+    catch(e) {
+      console.log(e);
+    }
+
+    return dicePoolData;
+  } else if(dicePoolObject.negDicePool) {
+    try {
+      // access posRes and negRes asynchronously, but wait until both are finished to continue (can be expanded to include force and nonDice if necessary)
+      let [negDicePoolData] = await Promise.all([getDoc(doc(db, 'dicePools', dicePoolObject.negDicePool))]);
+      
+      let rollPossibilities = dicePoolDataExists({negDicePoolData: negDicePoolData}, dicePoolObject)
+      dicePoolData = {posDicePoolObj: '', negDicePoolObj: rollPossibilities.negDicePoolData, forceDice: '', symbols: ''};
+    }
+    catch(e) {
+      console.log(e);
+    }
+
+    return dicePoolData;
+  }
 }
 
 // check if the dicePool exists in the database
 function dicePoolDataExists(dicePoolData, dicePoolObject) {
 
   let dicePoolDataObj = {}
-
-  // Check if positive roll is in the database
-  if (dicePoolData.posDicePoolData.exists()) {
-    // return the data if it exists
-    dicePoolDataObj.posDicePoolData = dicePoolData.posDicePoolData.data();
-  } else {
-    // create and return the data if it does not exist
-    dicePoolDataObj.posDicePoolData = getDicePoolObject(getFaceArrays(dicePoolObject).posFaceArray, 'positive');
-    console.log('PosDicePool Not Found')
-    console.log('PosDicePoolData:',dicePoolDataObj.posDicePoolData)
-    // then add that data to the database
-    createDicePoolDoc(dicePoolObject.posDicePool, dicePoolDataObj.posDicePoolData);
+  console.log('posDicePool:')
+  if(dicePoolData.posDicePoolData) {
+    console.log(dicePoolData.posDicePoolData)
+    // Check if positive roll is in the database
+    if (dicePoolData.posDicePoolData.exists()) {
+      // return the data if it exists
+      dicePoolDataObj.posDicePoolData = dicePoolData.posDicePoolData.data();
+    } else {
+      // create and return the data if it does not exist
+      dicePoolDataObj.posDicePoolData = getDicePoolObject(getFaceArrays(dicePoolObject).posFaceArray, 'positive');
+      console.log('PosDicePool Not Found')
+      console.log('PosDicePoolData:',dicePoolDataObj.posDicePoolData)
+      // then add that data to the database
+      createDicePoolDoc(dicePoolObject.posDicePool, dicePoolDataObj.posDicePoolData);
+    }
   }
 
-  // Check if negative roll is in the database
-  if (dicePoolData.negDicePoolData.exists()) {
-    // return the data if it is
-    dicePoolDataObj.negDicePoolData = dicePoolData.negDicePoolData.data();
-  } else {
-    // create and return the data if it is not
-    dicePoolDataObj.negDicePoolData = getDicePoolObject(getFaceArrays(dicePoolObject).negFaceArray, 'negative');
-    console.log('NegDicePool Not Found')
-    console.log('NegDicePoolData:',dicePoolDataObj.negDicePoolData)
-    // then add that data to the database
-    createDicePoolDoc(dicePoolObject.negDicePool, dicePoolDataObj.negDicePoolData)
+  console.log('negDicePool:')
+  if(dicePoolData.negDicePoolData) {
+    console.log(dicePoolData.negDicePoolData)
+    // Check if negative roll is in the database
+    if (dicePoolData.negDicePoolData.exists()) {
+      // return the data if it is
+      dicePoolDataObj.negDicePoolData = dicePoolData.negDicePoolData.data();
+    } else {
+      // create and return the data if it is not
+      dicePoolDataObj.negDicePoolData = getDicePoolObject(getFaceArrays(dicePoolObject).negFaceArray, 'negative');
+      console.log('NegDicePool Not Found')
+      console.log('NegDicePoolData:',dicePoolDataObj.negDicePoolData)
+      // then add that data to the database
+      createDicePoolDoc(dicePoolObject.negDicePool, dicePoolDataObj.negDicePoolData)
+    }
   }
+  
   console.log(dicePoolDataObj)
   return dicePoolDataObj;
 }
